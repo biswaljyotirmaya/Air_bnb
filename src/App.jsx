ee4407b350ccc7aaab8e0d0b8033cc0759f10664
biswaljyotirmaya <biswaljyotirmaya06@gmail.com>
2024-02-17 12:46:50 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Blog from "./pages/Blog";
-import BlogDetails from "./pages/BlogDetails";
-import PlacesRoute from "./pages/PlacesRoute";
 import SigninLogin from "./Shared/SigninLogin";
 import HelpCenter from "./Shared/HelpCenter";
 import Details from "./components/Deatails/Details";
@@ -17,9 +14,6 @@ const App = () => {
         <Route path="/" exact element={<Home />} />
         <Route path="/details" exact element={<Details/>} />
         <Route path="/about" exact element={<About />} />
-        <Route path="/blogs" exact element={<Blog />} />
-        <Route path="/blogs/:id" exact element={<BlogDetails />} />
-        <Route path="/places" exact element={<PlacesRoute />} />
         <Route path="/signinLogin" exact element={<SigninLogin />} />
         <Route path="/helpCenter" exact element={<HelpCenter />} />
         <Route path="/galary" exact element={<Galary/>} />
